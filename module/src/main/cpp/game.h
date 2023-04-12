2f6196d116d279e6942296cea2a4a6a42c557512
imgoodgamerbro <109311245+imgoodgamerbro@users.noreply.github.com>
2023-04-12 16:01:55 +0530

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.kitkagames.fallbuddies"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
